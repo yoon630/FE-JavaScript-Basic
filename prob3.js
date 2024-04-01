5dd312e2325f6a2a39fbc4c116baa5dd6816665d
yoon630 <choiyunseon630@gmail.com>
2024-04-01 23:25:29 +0900

    prob3

--- prob3.js
+++ prob3.js
@@ -1,15 +1,14 @@
 /* 3번 문제 답안을 작성해주세요. */
 function solution(arr) {
-  var answer = [];
-  if(arr.length =0){
-      answer = [-1];
-      return answer;
-  }
-  else if(arr.length > 0){
-    for(let i=0;i<arr.length;i++){
-        
+  const minValue = Math.min(...arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === minValue) {
+      const removedArr = arr.splice(i, 1);
+      if (arr.length === 0) {
+        return [-1];
+      } else {
+        return arr;
+      }
     }
-     anwer=arr.splice(작은거 잘라내는거)
   }
-  return answer;
 }
