6bf16826c579b450a4266777a729dfb6c711b6c2
yoon630 <choiyunseon630@gmail.com>
2024-04-01 22:53:17 +0900

    prob2

--- prob2.js
+++ prob2.js
@@ -1,1 +1,12 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(n) {
+  var answer = 1;
+  for (let i = 1; i <= 10; i++) {
+    answer *= i;
+    if (answer > n) {
+      return i - 1;
+    } else if (answer === n) {
+      return i;
+    }
+  }
+}
--- prob3.js
+++ prob3.js
@@ -1,1 +1,15 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+  var answer = [];
+  if(arr.length =0){
+      answer = [-1];
+      return answer;
+  }
+  else if(arr.length > 0){
+    for(let i=0;i<arr.length;i++){
+        
+    }
+     anwer=arr.splice(작은거 잘라내는거)
+  }
+  return answer;
+}
